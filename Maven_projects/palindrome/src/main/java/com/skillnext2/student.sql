e0ddda60672cd718e02afab08b38b9b180f28120
spavithrareddy25 <spavithrareddy25@gmail.com>
2025-12-17 09:44:49 +0530

    jfs projects

---
+++ Maven_projects/palindrome/src/main/java/com/skillnext2/student.sql
@@ -0,0 +1,17 @@
+CREATE TABLE student (
+    rollno INT PRIMARY KEY,
+    name VARCHAR(50),
+    subject VARCHAR(30),
+    marks INT
+);
+INSERT INTO student (rollno, name, subject, marks) VALUES
+(1,'Ravi','Maths',85),
+(2,'Sita','Physics',78),
+(3,'Amit','Chemistry',92),
+(4,'Priya','Maths',88),
+(5,'Kiran','Biology',69),
+(6,'Neha','Physics',74),
+(7,'Arjun','Chemistry',81),
+(8,'Divya','Maths',95),
+(9,'Manoj','Biology',34),
+(10,'Anjali','Physics',32);
